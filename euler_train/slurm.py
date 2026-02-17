abdfae3e720fb8ede370edcdedf7da3a0504d8d7
Daniel Rothenpieler <rothenpielerdaniel@gmail.com>
2026-02-17 23:41:10 +0100

    add meta-schema, more slurm tracking

--- euler_train/slurm.py
+++ euler_train/slurm.py
@@ -15,4 +15,13 @@ def get_slurm_info():
         "gpus": os.environ.get("SLURM_GPUS"),              # If allocated
         "cpus": os.environ.get("SLURM_CPUS_PER_TASK"),
         "array_task_id": os.environ.get("SLURM_ARRAY_TASK_ID"),  # If array job
+        "num_nodes": os.environ.get("SLURM_JOB_NUM_NODES"),
+        "ntasks": os.environ.get("SLURM_NTASKS"),
+        "ntasks_per_node": os.environ.get("SLURM_NTASKS_PER_NODE"),
+        "gpus_per_node": os.environ.get("SLURM_GPUS_PER_NODE"),
+        "mem_per_node": os.environ.get("SLURM_MEM_PER_NODE"),
+        "mem_per_cpu": os.environ.get("SLURM_MEM_PER_CPU"),
+        "stdout_path": os.environ.get("SLURM_JOB_STDOUT"),  # --output log
+        "stderr_path": os.environ.get("SLURM_JOB_STDERR"),  # --error log
+        "submit_dir": os.environ.get("SLURM_SUBMIT_DIR"),
     }
